0599a6ad8bbeea69e72f7869ca822baaa6d5e2e2
edmar.james <edmarjamesbautista@gmail.com>
2025-01-03 17:32:34 +0800

    Create InputField component showing how to use interface for props, use useState and define its type, create model.ts and add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import { useState } from 'react';
+import InputField from './components/InputField';
+import { Todo } from './model';
 import './App.css';
 
 function App() {
 
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}])
+      setTodo('');
+    };
+  };
+
+  console.log(todos);
+
   return (
     <div className='App'>
       <span className='heading'>Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   )
 }
