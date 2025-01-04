5c01a082f6996775277d621cb23072245d53f462
edmar.james <edmarjamesbautista@gmail.com>
2025-01-04 16:33:28 +0800

    Add done and delete functionalities

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -10,13 +10,28 @@ interface Props {
 };
 
 export default function SingleTodo({ todo, todos, setTodos }: Props) {
+
+  const handleDone = (id: number) => {
+    const done = todos.map((todo) => todo.id === id ? { ...todo, isDone: !todo.isDone }: todo);
+    setTodos(done);
+  };
+
+  const handleDelete = (id: number) => {
+    const filtered = todos.filter((todo) => todo.id !== id);
+    setTodos(filtered);
+  };
+
   return (
     <form className='todos__single'>
-      <span className='todos__single--text'>{todo.todo}</span>
+      {todo.isDone ? (
+        <s className='todos__single--text'>{todo.todo}</s>
+      ) : (
+        <span className='todos__single--text'>{todo.todo}</span>
+      )}
       <div>
         <span className="icon"><FaEdit /></span>
-        <span className="icon"><MdDelete /></span>
-        <span className="icon"><MdDone /></span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}><MdDelete /></span>
+        <span className="icon" onClick={() => handleDone(todo.id)}><MdDone /></span>
       </div>
     </form>
   )
